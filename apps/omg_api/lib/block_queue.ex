f77c54a7b54c34b9a459ea211d1220e20112a000
Piotr Dobaczewski Imapp <piotr.dobaczewski@imapp.pl>
2019-05-14 13:12:39 +0200

    feat: improve error logs when BlockQueue has problems
    Covers two common situations:
     - when BlockQueue can't start because the `OMG.DB` mismatches with the contract
     - when BlockQueue can't submit blocks because the authority address is locked
    
    When this happens we'll going to be building some relevant diagnostic data and including it in the error log.
    
    Should help with issues like #662

--- apps/omg_api/lib/block_queue.ex
+++ apps/omg_api/lib/block_queue.ex
@@ -83,7 +83,7 @@ defmodule OMG.API.BlockQueue do
 
       {:ok, known_hashes} = OMG.DB.block_hashes(range)
       {:ok, {top_mined_hash, _}} = Eth.RootChain.get_child_chain(mined_num)
-      _ = Logger.info("Starting BlockQueue, top_mined_hash: #{inspect(Base.encode16(top_mined_hash))}")
+      _ = Logger.info("Starting BlockQueue, top_mined_hash: #{inspect(Eth.Encoding.to_hex(top_mined_hash))}")
 
       {:ok, state} =
         with {:ok, _state} = result <-
@@ -102,8 +102,11 @@ defmodule OMG.API.BlockQueue do
         else
           {:error, reason} = error when reason in [:mined_hash_not_found_in_db, :contract_ahead_of_db] ->
             _ =
-              Logger.error(
-                "The child chain might have not been wiped clean when starting a child chain from scratch. Check README.MD and follow the setting up child chain."
+              log_init_error(
+                known_hashes: known_hashes,
+                parent_height: parent_height,
+                mined_num: mined_num,
+                stored_child_top_num: stored_child_top_num
               )
 
             error
@@ -167,6 +170,17 @@ defmodule OMG.API.BlockQueue do
       {:ok, newest_mined_blknum} = Eth.RootChain.get_mined_child_block()
 
       :ok = Core.process_submit_result(submission, submit_result, newest_mined_blknum)
+    end
+
+    defp log_init_error(fields) do
+      config = Application.get_all_env(:omg_eth) |> Keyword.take([:contract_addr, :authority_addr, :txhash_contract])
+      fields = Keyword.update!(fields, :known_hashes, fn hashes -> Enum.map(hashes, &Eth.Encoding.to_hex/1) end)
+      diagnostic = fields |> Enum.into(%{config: config})
+
+      Logger.error(
+        "The child chain might have not been wiped clean when starting a child chain from scratch: " <>
+          "#{inspect(diagnostic)}. Check README.MD and follow the setting up child chain."
+      )
     end
   end
 end
